7fd15d0f0926f4ba55ee498bb739f7153babeef5
Anthony Graddy <anthony@locationq.com>
2016-12-30 06:30:21 +0000

    Handle arrays with nested values

--- test/index.js
+++ test/index.js
@@ -3,6 +3,8 @@ var response = require('agraddy.test.res');
 var pack = require('../package.json');
 var stream = require('stream');
 
+var util = require('util');
+
 var mod = require('../');
 
 var req = new stream.Readable();
@@ -18,6 +20,7 @@ mod.luggage(req, res, lug, function() {
 
 tap.assert.equal(pack.luggage, true, 'The luggage item needs to be set in the package.json file.');
 
+
 var req2 = new stream.Readable();
 req2._read = function(size) {
 	this.push('id=1&from[]=TX&to[]=AL&from[]=&to[]=&from[]=&to[]=&from[]=&to[]=&from[]=&to[]=');
@@ -27,5 +30,18 @@ var res2 = response();
 var lug2 = {};
 mod.luggage(req2, res2, lug2, function() {
 	tap.assert.deepEqual(lug2.post, {"from": ["TX", "", "", "", ""], "id": "1", "to": ["AL", "", "", "", ""]}, 'Should set post with arrays.');
+});
+
+
+var req3 = new stream.Readable();
+req3._read = function(size) {
+	this.push('deep.down.obj=test&deep.down.another=second&id=1&filter_json[].from=TX&filter_json[].to=AL&filter_json[].from=&filter_json[].to=&filter_json[].from=&filter_json[].to=&filter_json[].from=&filter_json[].to=&filter_json[].from=&filter_json[].to=&arr[].with.extra.depth=one&arr[].with.extra.width=one.five&arr[].with.extra.depth=two&arr[].with.extra.width=two.five');
+	this.push(null);
+};
+var res3 = response();
+var lug3 = {};
+mod.luggage(req3, res3, lug3, function() {
+	console.log(util.inspect(lug3.post, false, null));
+	tap.assert.deepEqual(lug3.post, {"arr": [{"with": {"extra": {"depth": "one", "width": "one.five"}}}, {"with": {"extra": {"depth": "two", "width": "two.five"}}}], "deep": {"down": {"another": "second", "obj": "test"}}, "filter_json": [{"from": "TX", "to": "AL"}, {"from": "", "to": ""}, {"from": "", "to": ""}, {"from": "", "to": ""}, {"from": "", "to": ""}], "id": "1"}, 'Should handle arrays with nested values.');
 });
 
